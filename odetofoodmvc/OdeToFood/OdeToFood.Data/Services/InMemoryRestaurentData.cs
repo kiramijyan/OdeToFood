9c6e091b2f272666d737c0e7f03b14f1c11aaf14
hovo <hkiramijyan@gmail.com>
2020-07-03 16:52:52 +0200

    Added fake data for restaurents

---
+++ odetofoodmvc/OdeToFood/OdeToFood.Data/Services/InMemoryRestaurentData.cs
@@ -0,0 +1,26 @@
+﻿using OdeToFood.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OdeToFood.Data.Services
+{
+    class InMemoryRestaurentData : IRestaurantData
+    {
+        List<Restaurant> restaurants;
+
+        public InMemoryRestaurentData()
+        {
+            restaurants = new List<Restaurant>();
+            {
+                new Restaurant { Id = 1, Name = "La casa Vito Morreale", Cuisine = CuisineType.Italian};
+                new Restaurant { Id = 2, Name = "Le Poivron Bleu", Cuisine = CuisineType.French };
+                new Restaurant { Id = 3, Name = "Le Karachi", Cuisine = CuisineType.Indian };
+            }
+        }
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return restaurants.OrderBy(r => r.Name);
+        }
+    }
+}
